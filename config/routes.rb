b26dbf33e5fa4ce728f0fd04ec90fafe6ab51b0c
quoctv1202 <quoctv1202@gmail.com>
2018-09-24 14:54:22 +0700

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'greetings/hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#hello"
 end
